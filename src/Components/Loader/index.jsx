1c16c302ccedd4156bba2c7d781c2cc99ca9378f
achT1205 <achilletuglo12@gmail.com>
2022-08-14 15:43:31 +0200

    commit

--- src/Components/Loader/index.jsx
+++
@@ -1,9 +0,0 @@
-import "./index.module.css";
-
-const Loader = () => (
-  <div id="overlay">
-    <div className="loader"></div>
-  </div>
-);
-
-export default Loader;
--- src/constants/abi.js
+++
@@ -1,4 +0,0 @@
-// @ts-nocheck
-import recruit  from "../assets/artifacts/EMRecruit.json";
-
-export const recruitABI = recruit.abi;
